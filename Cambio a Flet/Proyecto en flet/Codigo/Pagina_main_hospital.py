3927b4db5e1e26e611a84e8afab80dbcbf597b2b
DavidAlejandroLibreros <160493533+DavidAlejandroLibreros@users.noreply.github.com>
2024-05-04 22:53:44 -0500

    Pantalla principal aplicación

---
+++ Cambio a Flet/Proyecto en flet/Codigo/Pagina_main_hospital.py
@@ -0,0 +1,74 @@
+import flet as ft
+from flet import FontWeight
+from Pantalla_citas import VerPantallaCitas
+
+def main(page: ft.Page):
+
+    page.fonts = {
+        "Times New Roman": "Fonts/Freeman-Regular.ttf"
+    }
+    def Ir_Agendar_citas(e):
+        page.views.clear()
+        page.views.append(VerPantallaCitas(page))
+    def Ir_conocer_personal_hospital(e):
+        page.views.clear()
+        page.views.append(())
+    
+    page.title = "Página principal del hospital"
+    page.appbar = ft.AppBar(
+        leading=ft.Icon(ft.icons.LOCAL_HOSPITAL_ROUNDED),
+        leading_width=50,
+        title=ft.Text("Hospital Fontibón E.S.E", color=ft.colors.BLACK, style = ft.TextStyle(font_family = "Times New Roman"), weight=ft.FontWeight.BOLD),
+        center_title=True,
+        bgcolor=ft.colors.GREEN_800,
+        actions=[
+            ft.SubmenuButton(
+                content=ft.Text("Conoce más sobre el hospital aquí", color = ft.colors.BLACK, bgcolor=ft.colors.WHITE, style= ft.TextStyle(font_family="Times New Roman"), weight=ft.FontWeight.BOLD),
+                controls=[
+                    ft.MenuItemButton(
+                        content=ft.Text("Conoce nuestro equipo"),
+                        leading=ft.Icon(ft.icons.PERSON),
+                        style=ft.ButtonStyle(bgcolor={ft.MaterialState.HOVERED: ft.colors.BLUE}),
+                        on_click=Ir_Agendar_citas),
+                    ft.MenuItemButton(
+                        content=ft.Text("Conoce nuestras instalaciones"),
+                        leading=ft.Icon(ft.icons.COLORIZE),
+                        style=ft.ButtonStyle(bgcolor={ft.MaterialState.HOVERED: ft.colors.GREEN})
+                    ),
+                    ft.MenuItemButton(
+                        content=ft.Text("Conoce nuestra historia"),
+                        leading=ft.Icon(ft.icons.COLORIZE),
+                        style=ft.ButtonStyle(bgcolor={ft.MaterialState.HOVERED: ft.colors.RED})
+                    )
+                ]
+            ),
+        ]
+    )
+    
+    page.add(
+        ft.Row(
+            [
+                ft.Container(
+                    content=ft.Column(
+                        [
+                            ft.ElevatedButton(content=ft.Text("Agende sus citas aquí"),on_click=Ir_Agendar_citas),
+                            ft.ElevatedButton("Consulte sus citas aquí"),
+                            ft.ElevatedButton("Otro botón"),
+                        ],
+                        spacing=15,
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                    ),
+                    margin=10,
+                    padding=10,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.colors.BLACK,
+                    width=220,
+                    height=500,
+                    border_radius=0,
+                ),
+            ]
+        )
+    )
+
+
+ft.app(target=main)
